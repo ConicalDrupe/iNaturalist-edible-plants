343b3d9ed6d3ca90970cacf8ef5e7aedf9b96383
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2025-07-05 15:09:03 -0400

    Added argsparser for genus or species matching

--- apis/gbif_key_matching_service.py
+++ apis/gbif_key_matching_service.py
@@ -1,18 +1,37 @@
 import pandas as pd
 import json
+import argparse
 import requests
 import datetime
 import gbif_password as g
 import os
 
+parser = argparse.ArgumentParser(prog='GBIF Key Matching Service',
+                                 description='Matches Scientific Names to GBIF usage keys')
+
+parser.add_argument('-s','--species',help='Species Rank',action=argparse.BooleanOptionalAction)
+parser.add_argument('-g','--genus',help='Genus Rank',action=argparse.BooleanOptionalAction)
+
+args = parser.parse_args()
+
 def match(name):
     url = "https://api.gbif.org/v1/species/match"
 
-    params = {
-        "name":name,
-        "rank": "SPECIES",
-        "kingdom":"Plantae",
-    }
+    if args.species:
+        params = {
+            "name":name,
+            "rank": "SPECIES",
+            "kingdom":"Plantae",
+        }
+    elif args.genus:
+        params = {
+            "name":name,
+            "rank": "GENUS",
+            "kingdom":"Plantae",
+        }
+    else:
+        print('-s or -g flag required')
+        exit()
 
     response = requests.get(url, params=params,auth=(g.user,g.p))
 
@@ -55,18 +74,29 @@ def save_matches(record_ls,save_path):
     print(f'[INFO] Saved matched names to {save_path}')
     return True
 
-def run_match_service(file_to_match='/home/ubuntu/iNaturalist-edible-plants/bookscraping/cleaning/edible_wild_IM.csv',save_dir='/home/ubuntu/iNaturalist-edible-plants/outputs'):
+def run_match_service(file_to_match='/home/boon/Projects/iNaturalist-edible-plants/bookscraping/outputs/reprocessing/final_665_species.csv',save_dir='/home/boon/Projects/iNaturalist-edible-plants/outputs',name_col='name'):
     dt = datetime.datetime.now()
     timestamp = dt.strftime("%Y-%m-%d_%H-%M-%S")
 
     df = pd.read_csv(file_to_match)
 
-    scientificName_ls = df["scientific_name"].to_list()
+    # Arg parser
+    df['name_len'] = df[name_col].apply(lambda x: len(x.split(' ')))
+    if args.species:
+        df = df[df['name_len']>=2]
+        file_flag='species'
+    elif args.genus:
+        df = df[df['name_len']==1]
+        file_flag='genus'
+    else:
+        print('-s or -g flag required')
+        exit()
+
+    scientificName_ls = df[name_col].to_list()
     print('Number of scientific names:',len(scientificName_ls))
 
-    save_path = os.path.join(save_dir,f'gbif_search_service_output_{len(scientificName_ls)}_{timestamp}.csv')
+    save_path = os.path.join(save_dir,f'gbif_search_service_output_{len(scientificName_ls)}_{file_flag}_{timestamp}.csv')
     all_records = match_all(scientificName_ls)
-    print("Saving file...\nfile:\n",save_path)
     save_matches(all_records,save_path)
     return save_path
 
