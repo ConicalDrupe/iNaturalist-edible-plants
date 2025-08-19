f4fdbf32cd8c51fea2e83de2139b513bd72d8c36
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2025-08-19 14:46:32 -0400

    Added occurrence point, with gist index for QGIS

--- dbt_inat/models/plants/fct_observations.sql
+++ dbt_inat/models/plants/fct_observations.sql
@@ -1,3 +1,10 @@
+{{ config(
+    materialized='table',
+    indexes=[
+        {'columns': ['occ_point'], 'type':'gist'}
+    ]
+) 
+}}
 
 with reverse_geocoded as 
 (
@@ -17,6 +24,7 @@ with reverse_geocoded as
     ,obs.identification_date
     ,obs.lon
     ,obs.lat
+    ,ST_SetSRID(ST_MakePoint(obs.lon,obs.lat),4326) as occ_point
     ,obs.occurrence_link
     ,obs.rights_holder
     ,ST_DISTANCE(obs.geo_loc,c.geom) as calculated_distance
@@ -42,6 +50,7 @@ SELECT
     ,identification_date
     ,lon
     ,lat
+    ,occ_point
     ,occurrence_link
     ,rights_holder
 from reverse_geocoded
